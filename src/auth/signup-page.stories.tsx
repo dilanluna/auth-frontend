d6d52830790238291b09afaaa87a5698bbaf024b
Dylan Luna <me@dilanluna.com>
2022-11-10 12:01:18 -0600

    feat: add sign up page

---
+++ src/auth/signup-page.stories.tsx
@@ -0,0 +1,19 @@
+import { withRouter } from 'storybook-addon-react-router-v6';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SignupPage from './signup-page';
+
+export default {
+  title: 'Auth/SignupPage',
+  component: SignupPage,
+  decorators: [withRouter],
+  parameters: {
+    layout: 'fullscreen',
+    reactRouter: {
+      routePath: '/signup',
+    },
+  },
+} as ComponentMeta<typeof SignupPage>;
+
+const Template: ComponentStory<typeof SignupPage> = () => <SignupPage />;
+
+export const Default = Template.bind({});
