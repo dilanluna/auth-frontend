07a639a2e84d2fca93b077488102f7d289e516ad
Dylan Luna <me@dilanluna.com>
2022-11-15 12:29:56 -0600

    feat: add signup thanks page

---
+++ src/auth/signup-thanks-page.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SignupThanksPage from './signup-thanks-page';
+
+export default {
+  title: 'Auth/SignupThanksPage',
+  component: SignupThanksPage,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof SignupThanksPage>;
+
+const Template: ComponentStory<typeof SignupThanksPage> = () => (
+  <SignupThanksPage />
+);
+
+export const Default = Template.bind({});
