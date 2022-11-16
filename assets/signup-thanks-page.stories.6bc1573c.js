489a20bcb5b63cd173049dfbbd947428efa2455a
dilanluna <dilanluna@users.noreply.github.com>
2022-11-16 21:49:03 +0000

    Deploying to storybook from @ dilanluna/auth-frontend@d8dc3a64b60bcd8c803dcfeedf1271f3f33d051d 🚀

---
+++ assets/signup-thanks-page.stories.6bc1573c.js
@@ -0,0 +1,18 @@
+var i=Object.defineProperty;var n=(o,s)=>i(o,"name",{value:s,configurable:!0});import{r as c}from"./react.076eb63d.js";import{a as t,j as e}from"./jsx-runtime.66a2b1fd.js";import"./iframe.4393ae2d.js";const r="/auth-frontend/assets/check.a8bf1d61.svg";function a(){return t("div",{className:"px-6 m-auto max-w-lg",children:[e("div",{className:"text-center",children:e("img",{src:c,alt:"React logo",className:"box-content inline-block p-[1.5em] will-change-[filter] animate-[spin_infinite_20s_linear] h-[6em] hover:drop-shadow-[0_0_2em_#61dafbaa]"})}),t("div",{className:"flex flex-col items-center justify-center mt-24 sm:mt-32",children:[e("img",{src:r,alt:"Completed",className:"mb-8"}),t("div",{className:"mb-7",children:[e("h4",{className:"text-neutral-600 text-xl font-semibold text-center",children:"Thanks for sign up!"}),e("p",{className:"text-neutral-600 text-center",children:"Check your email to enable your account"})]}),e("button",{className:"bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:bg-blue-700 text-white font-medium py-[10px] px-4 rounded-md focus:ring focus:ring-blue-500 focus:ring-offset-1 min-w-[205px]",children:"Continue"})]})]})}n(a,"SignupThanksPage");const h={title:"Auth/SignupThanksPage",component:a,parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SignupThanksPage from './signup-thanks-page';
+
+export default {
+  title: 'Auth/SignupThanksPage',
+  component: SignupThanksPage,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof SignupThanksPage>;
+
+const Template: ComponentStory<typeof SignupThanksPage> = () => (
+  <SignupThanksPage />
+);
+
+export const _SignupThanksPage = Template.bind({});
+`,locationsMap:{"signup-thanks-page":{startLoc:{col:58,line:12},endLoc:{col:1,line:14},startBody:{col:58,line:12},endBody:{col:1,line:14}}}},layout:"fullscreen"}},l=n(()=>e(a,{}),"Template"),d=l.bind({}),f=["_SignupThanksPage"];export{d as _SignupThanksPage,f as __namedExportsOrder,h as default};
+//# sourceMappingURL=signup-thanks-page.stories.6bc1573c.js.map
