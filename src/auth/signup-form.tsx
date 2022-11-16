d0f565279c6739867fd8a13902894a92351907ae
Dylan Luna <me@dilanluna.com>
2022-11-16 13:54:36 -0600

    refactor: extract label component in its own file

--- src/auth/signup-form.tsx
+++ src/auth/signup-form.tsx
@@ -2,27 +2,11 @@ import clsx from 'clsx';
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
 import { ErrorMessage } from '@hookform/error-message';
-import { forwardRef, InputHTMLAttributes, LabelHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
+import Label from '../components/label';
 import Feedback from '../components/feedback';
 
 const Joi = await import('joi');
-
-function Label({
-  children,
-  className,
-  ...props
-}: LabelHTMLAttributes<HTMLLabelElement>) {
-  return (
-    <label
-      {...props}
-      className={clsx(
-        'text-sm font-medium text-gray-400 peer-placeholder-shown:text-neutral-600 peer-invalid:text-red-500',
-        className,
-      )}>
-      {children}
-    </label>
-  );
-}
 
 const Input = forwardRef<
   HTMLInputElement,
