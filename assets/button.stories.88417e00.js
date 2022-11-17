dd5153b67d9701aa2d8f243ccb6d087b190742ce
dilanluna <dilanluna@users.noreply.github.com>
2022-11-17 19:08:54 +0000

    Deploying to storybook from @ dilanluna/auth-frontend@aa3df99cd36b75006db0400e1a45f79f9bc95624 🚀

---
+++ assets/button.stories.88417e00.js
@@ -0,0 +1,15 @@
+var r=Object.defineProperty;var o=(t,e)=>r(t,"name",{value:e,configurable:!0});import{B as n}from"./button.4f8a4494.js";import{j as p}from"./jsx-runtime.32419ef3.js";import"./clsx.m.256e9345.js";import"./iframe.28da72ae.js";const l={parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Button from './button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (props) => (
+  <Button {...props}>Button</Button>
+);
+
+export const _Button = Template.bind({});
+`,locationsMap:{button:{startLoc:{col:48,line:9},endLoc:{col:1,line:11},startBody:{col:48,line:9},endBody:{col:1,line:11}}}}},title:"Components/Button",component:n},s=o(t=>p(n,{...t,children:"Button"}),"Template"),B=s.bind({}),d=["_Button"];export{B as _Button,d as __namedExportsOrder,l as default};
+//# sourceMappingURL=button.stories.88417e00.js.map
