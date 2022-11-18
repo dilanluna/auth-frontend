f6f3fc91793ce332d11811c5b593f1ac3984277b
Dylan Luna <me@dilanluna.com>
2022-11-18 09:20:53 -0600

    chore: setup path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
@@ -12,6 +13,7 @@ export default defineConfig({
       typescript: true,
     }),
     topLevelAwait(),
+    tsconfigPaths(),
   ],
   test: {
     globals: true,
